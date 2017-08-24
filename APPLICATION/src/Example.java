d1f18b7eed122e1f048b78498bd604a1f7a23b9b
KRISHNA <maddelakrishnakumar@gmail.com>
2017-08-24 12:46:18 +0530

    First Commit of example

---
+++ APPLICATION/src/Example.java
@@ -0,0 +1,17 @@
+
+public class Example {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String[] names= {"Joy","josh","thanuja","krishna"};
+		
+		for(String s:names)
+		{
+			System.out.println("Greeting of the Day :"+s);
+		}
+
+	}
+
+}
